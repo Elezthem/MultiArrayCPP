e9f0b70f3dce4f31238f3d62e3752e9459393843
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-09-27 07:00:36 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	
+	int multi[4][3] = {
+		{6, 7, 4},
+		{8, 10, 2},
+		{1, 5, 4},
+		{2, 3, 4}
+	};
+	/*multi[1][0] = 500;
+	cout << multi[1][0] << endl;*/
+
+	for (int a = 0; a < 4; a++) {
+		for (int s = 0; s < 3; s++) {
+			cout << multi[a][s] << endl;
+		}
+	}
+
+	return 0;
+}
